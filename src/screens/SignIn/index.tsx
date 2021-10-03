a089743ba594cf3a79f4ed4356f5dbe7e7c5f1e0
tiagoolivv <suubmundo@gmail.com>
2021-10-03 11:00:16 -0300

    feat: signin screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { SignInSocialButton } from '../../components/SignInSocialButton';
+
+import { 
+  Container, 
+  Header,
+  TitleWrapper,
+  Title,
+  SignInTitle,
+  Footer,
+  FooterWrapper
+} from './styles';
+
+import AppleSvg from '../../assets/apple.svg';
+import GoogleSvg from '../../assets/google.svg';
+import LogoSvg from '../../assets/logo.svg';
+
+export function SignIn() {
+  return (
+    <Container>
+      <Header>
+        <TitleWrapper>
+          <LogoSvg width={RFValue(120)} height={RFValue(68)} />
+          <Title>
+            Controle suas{`\n`}
+            finanças de forma{`\n`}
+            muito simples
+          </Title>
+        </TitleWrapper>
+        <SignInTitle>
+          Faça seu login com{`\n`}
+          uma das contas abaixo
+        </SignInTitle>
+      </Header>
+      <Footer>
+        <FooterWrapper>
+          <SignInSocialButton 
+            svg={GoogleSvg}
+            title="Entrar com Google"
+          />
+          <SignInSocialButton 
+            svg={AppleSvg} 
+            title="Entrar com Apple"
+          />
+        </FooterWrapper>
+      </Footer>
+    </Container>
+  );
+};
